f601064994f5a063373f4d502cda52a3934704ee
Lyxn-M <smthelynxs@gmail.com>
2024-08-01 03:34:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function contactTeam(productName) {
+    alert(`Thank you for your interest in ${productName}. Please contact our team at the following numbers: +20 106 351 5106 +20 102 966 5151 +20 109 356 5359`);
+}
