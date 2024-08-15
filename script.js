a35d5921ca83d6f33943ee71c2a9a138c7bae7a5
Boxi <smthelynxs@gmail.com>
2024-08-15 19:42:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function contactTeam(productName) {
     alert(`Thank you for your interest in ${productName}. Please contact our team at the following numbers: +20 106 351 5106 +20 102 966 5151 +20 109 356 5359`);
 }
+
+let counter = 400; 
+const goal = 500; 
+const progressBar = document.getElementById('progress');
+const counterElement = document.getElementById('counter');
+
+
+function updateCounter() {
+  counterElement.textContent = counter;
+  const progressWidth = (counter / goal) * 100;
+  progressBar.style.width = `${progressWidth}%`;
+}
+
+
+function incrementCounter() {
+  counter++;
+  updateCounter();
+}
+
+
+counter = 0;
+updateCounter();
