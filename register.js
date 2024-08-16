5fc6f7366a8ee8457e44a9f466861a8584d47dcf
Boxi <smthelynxs@gmail.com>
2024-08-16 17:08:47 +0300

    Add files via upload

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGTUb7ggDaGKHzN-m9OmX21RH-JPMo9gk",
+  authDomain: "boxi-for-it.firebaseapp.com",
+  projectId: "boxi-for-it",
+  storageBucket: "boxi-for-it.appspot.com",
+  messagingSenderId: "166081574527",
+  appId: "1:166081574527:web:58e9e2fbcbc47757a0714a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
